97a0f8a9832bce5407a4587e631d1b4b72cee2c8
Chen-shuyang <99723175+Chen-Shu-Yang@users.noreply.github.com>
2023-02-24 15:51:06 +0800

    edits

--- FrontEnd/public/common.js
+++ FrontEnd/public/common.js
@@ -1,1 +1,5 @@
-const serverName = 'http://localhost:5000';
+// For Development
+// const serverName = 'http://localhost:5000';
+
+// For Deployment on Heroku
+const serverName = 'https://csyportfolio.herokuapp.com';
